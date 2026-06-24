f95f1303d3abaeabaf2bff82a36e178615274338
aScarecrow000 <alone.scarecrow.171@gmail.com>
2026-06-24 12:42:24 +0330

    update: updated argparse script and added verbose and optional arguments

--- argparse-test.py
+++ argparse-test.py
@@ -1,10 +1,24 @@
 import argparse
 
-# create the parser
-arg_parser = argparse.ArgumentParser(description="This is a test script for argparse.")
-arg_parser.add_argument("mau", help="This is the main argument.")
-# recall the arguments
-arg_parser.parse_args()
-args = arg_parser.parse_args()
+# 1. Create the master parser
+mau = argparse.ArgumentParser()
 
-print(f"The value of the main argument 'mau' is: {args.mau}")
+# 2. Register the arguments (slots) here
+mau.add_argument("echo", help="echo the string you use here")
+mau.add_argument(
+    "--square", help="gives you the sqaure of the number you use here", type=int
+)  # making it optional with --square
+mau.add_argument(
+    "--verbose", help="increase output verbosity", action="store_true"
+)  # adding a flag for verbosity, a true/false switch
+
+# 3. Parse the arguments
+args = mau.parse_args()
+
+# 4. Use conditions to run my practice blocks
+print(f"echo argument parse: {args.echo}")
+
+if args.square is not None:
+    print(f"square argument parse: {args.square**2}")
+if args.verbose:
+    print("verbosity turned on")
